c7e51ed0bbeac42fec9cea10995adf9f166920f9
rixong <rixong@gmail.com>
2020-03-29 16:43:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+
+
 class Formatter {
-  //add static methods here
+
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+
+  static titleize(str) {
+    let arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let splitArr = str.split(' ');
+    let res = splitArr.map((word) => {
+      if (arr.includes(word)) {
+        return word;
+      } else {
+        return this.capitalize(word);
+      }
+    })
+    res[0] = this.capitalize(res[0]);
+    return res.join(' ');
+  }
+
 }
